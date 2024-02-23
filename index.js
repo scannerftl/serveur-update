4e9d3d6d6e67d760246d1ab0bc337b00ae25eeae
leonel <leonelfofou98@gmail.com>
2024-02-23 08:49:50 +0100

    tesr release

--- index.js
+++ index.js
@@ -7,11 +7,8 @@ const path = require('path');
 // const server = 'https://hazel-nine-mu.vercel.app';
 // const link = `${server}/update/${process.platform}/${app.getVersion()}`;
 // autoUpdater.setFeedURL(link);
-// Logique de mise à joure automatique 
-setInterval(() => {
-	// autoUpdater.checkForUpdates()
-	autoUpdater.checkForUpdatesAndNotify();
-  }, 60000)
+
+autoUpdater.checkForUpdatesAndNotify();
 
 // interface graphique 
 function onReady () {     
@@ -28,26 +25,5 @@ function onReady () {
 	}))   
 }
 
-
-autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-	const dialogOpts = {
-	  type: 'info',
-	  buttons: ['Restart', 'Later'],
-	  title: 'Application Update',
-	  message: process.platform === 'win32' ? releaseNotes : nomrelease,
-	  détail:
-		'Une nouvelle version a été téléchargée. Redémarrez l\'application pour appliquer les mises à jour.'
-	}
-  
-	dialog.showMessageBox(dialogOpts).then((returnValue) => {
-	  if (returnValue.response === 0) autoUpdater.quitAndInstall()
-	})
-  })
-
-autoUpdater.on('error', (message) => {
-	console.error('There was a problem updating the application ')
-	console.error(message)
-  })
-
-  // lancer l'application 
+// lancer l'application 
 app.on('ready', onReady);
